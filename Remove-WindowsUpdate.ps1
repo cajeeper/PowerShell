07dbb305edec36dca58432033affcb728d6672e2
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-12-01 11:07:53 -0800

    Master

--- Remove-WindowsUpdate.ps1
+++ Remove-WindowsUpdate.ps1
@@ -10,22 +10,33 @@
   Date     : 2015-12-01  
   Contact  : http://www.allthingstechie.net
   Revision : v1  
+
+ .EXAMPLE 
+  C:\PS> #Uninstall One Update 
+  C:\PS> Remove-WindowsUpdate 123456
+  
+ .EXAMPLE 
+  C:\PS> #Uninstall Multiple Updates
+  C:\PS> Remove-WindowsUpdate 123456,456123
 #>
+Function Remove-WindowsUpdate {
+    [CmdletBinding()]  
+      param (  
+           [parameter(Mandatory=$True)] $RemoveKB
+		   )
 
-#$RemoveKB = "123456","456123"
-$RemoveKB = "123456"
+	$Searcher = New-Object -ComObject Microsoft.Update.Searcher
+	$RemoveCollection = New-Object -ComObject Microsoft.Update.UpdateColl
 
-$Searcher = New-Object -ComObject Microsoft.Update.Searcher
-$RemoveCollection = New-Object -ComObject Microsoft.Update.UpdateColl
+	#Gather All Installed Updates
+	$SearchResult = $Searcher.Search("IsInstalled=1")
 
-#Gather All Installed Updates
-$Result = $Searcher.Search("IsInstalled=1")
+	#Add any of the specified KBs to the RemoveCollection
+	$SearchResult.Updates | ? { $_.KBArticleIDs -in $RemoveKB } | % { $RemoveCollection.Add($_) }
 
-#Add any of the specified KBs to the RemoveCollection
-$Result.Updates | ? { $_.KBArticleIDs -in $RemoveKB } | % { $RemoveCollection.Add($_) }
-
-if ($RemoveCollection.Count -gt 0) {
-$Installer = New-Object -ComObject Microsoft.Update.Installer
-$Installer.Updates = $RemoveCollection
-$Installer.Uninstall()
-} else { Write-Warning "No matching Windows Updates found for:`n$($RemoveKB|Out-String)" }
+	if ($RemoveCollection.Count -gt 0) {
+		$Installer = New-Object -ComObject Microsoft.Update.Installer
+		$Installer.Updates = $RemoveCollection
+		$Installer.Uninstall()
+	} else { Write-Warning "No matching Windows Updates found for:`n$($RemoveKB|Out-String)" }
+}
