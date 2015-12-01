8128b8c566f2a835f1db9d11743a0389450ba39f
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-12-01 11:50:30 -0800

    Master

---
+++ Office365/Remove-EmailFromExternalSender.ps1
@@ -0,0 +1,15 @@
+$LiveCred = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session
+
+$mailboxes = "userA@myDomain.com","userB@myDomain.com"
+
+#Save what was discovery first
+$mailboxes | % {
+	get-Mailbox $_ | Search-Mailbox -SearchQuery { From:"userX@extDomain.com" and Sent:"10/14/2015"} -TargetFolder  "userX" -TargetMailbox discoveryMailBox@myDomain.com
+	}
+	
+#Now, delete the email in question
+$mailboxes | % { Get-Mailbox $_ | Search-Mailbox -SearchQuery { From:"userX@extDomain.com" and Sent:"10/14/2015" } -DeleteContent -Confirm:$false	}
