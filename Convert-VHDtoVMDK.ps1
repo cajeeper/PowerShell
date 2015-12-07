f3f2d5a5ab3c0675f668ecf6d9f587d12ef26389
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-12-07 14:18:50 -0800

    Master

--- Convert-VHDtoVMDK.ps1
+++ Convert-VHDtoVMDK.ps1
@@ -9,9 +9,11 @@
  
  .NOTES   
   Author   : Justin Bennett   
-  Date     : 2015-12-03 
+  Date     : 2015-12-07
   Contact  : http://www.allthingstechie.net
-  Revision : v1  
+  Revision : v1.1 
+  Changes  : v1.0 Original
+			 v1.1 Added starwindsConvert, vmdkAdapterType, and vmdkType parameters
    
  .PARAMETER convertFromDir  
   Name of directory location containing VHD/VHDX files
@@ -19,6 +21,16 @@
  .PARAMETER convertFromTo
   Name of directory location where VMDK files will be deposited
   
+ .PARAMETER vmdkAdapterType
+  Set vmdk disk adapter to IDE, SCSI, or FirstIDE to first disk IDE with subsequential vmdk disks set to SCSI
+  
+ .PARAMETER vmdkType
+  Set vmdk disk type to VMDK_F - VMWare pre-allocated image, VMDK_S - VMWare growable image,
+	VMDK_SO - VMWare stream-optimized image, or VMDK_VMFS - VMWare ESX server image.
+ 
+ .PARAMETER starwindsConvert
+  File path of the StarWindows Converter Executable, StarV2Vc.exe 
+ 
  .EXAMPLE  
   C:\PS> #Convert files in C:\DirA to C:\DirB
   C:\PS> Convert-VHDtoVMDK -convertFromDir 'C:\DirA' -convertToDir 'C:\DirB'
@@ -26,25 +38,23 @@
 function Convert-VHDtoVMDK {
      [CmdletBinding()]  
       param (  
-           [parameter(Mandatory=$True)] $convertFromDir,
-		   [parameter(Mandatory=$True)] $convertToDir
+           [parameter(Mandatory=$True)] [ValidateScript({Test-Path $_})] [string] $convertFromDir,
+		   [parameter(Mandatory=$True)] [ValidateScript({Test-Path $_})] [string] $convertToDir,
+		   [parameter(Mandatory=$False)] [ValidateSet("IDE","SCSI","FirstIDE")] [string] $vmdkAdapterType = "SCSI",
+		   [parameter(Mandatory=$False)] [ValidateSet("VMDK_F","VMDK_S","VMDK_SO","VMDK_VMFS")] [string] $vmdkType = "VMDK_F",
+		   [parameter(Mandatory=$False)] [ValidateScript({Test-Path $_})] [string] $starwindsConvert = 'C:\Program Files (x86)\StarWind Software\StarWind V2V Image Converter\StarV2Vc.exe'
 		   )
-	#Conversion Exe
-	$starwindsConvert = 'C:\Program Files (x86)\StarWind Software\StarWind V2V Image Converter\StarV2Vc.exe'
 	
 	#initial vmdk set to 
-	$vmdkadtype = "IDE"
-	$vmdkfiletype = "vmdk_s"
-	
-	if(!(Test-Path $convertFromDir)) { write-warning "Directory $convertFromDir was not found."; break;}
-	if(!(Test-Path $convertToDir)) { write-warning "Directory $convertToDir was not found."; break; }
+	if($vmdkAdapterType -eq "SCSI") { $vmdkadtype = "SCSI" } else { $vmdkadtype = "IDE" }
+	$vmdkType = "vmdk_s"
 	
 	$files = Get-ChildItem -Path $convertFromDir -Filter *.vhd*
 	
 	foreach ($file in $files) {
 		$i++
 		$start = get-date
-		$run = "`& `"$($starwindsConvert)`" if=`"$($file.fullname)`" of=`"$($convertToDir)\$($file.basename).vmdk`" ot=$($vmdkfiletype) vmdktype=$($vmdkadtype)"
+		$run = "`& `"$($starwindsConvert)`" if=`"$($file.fullname)`" of=`"$($convertToDir)\$($file.basename).vmdk`" ot=$($vmdkType) vmdktype=$($vmdkadtype)"
 		#run conversion
 		Write-Progress -Activity "Converting VHD(X) to VMDK: From $($convertFromDir), To $($convertToDir)" -Status "File $($i)/$($files.count): $($file.name), Started at $(get-date -Format g $start)"
 		try { $output = Invoke-Expression $run } catch { $output = "Failed to convert file $($file.name), Error $($_)"; write-warning $output; }
@@ -57,9 +67,9 @@ function Convert-VHDtoVMDK {
 			"End"= $end
 		})
 		
-		#subsequential vmdk set to SCSI
-		$vmdktype = "SCSI"
+		#subsequential vmdk set to SCSI if FirstIDE set
+		if($vmdkAdapterType -eq "FirstIDE") { $vmdkadtype = "SCSI" }
 	}
 	
-	if($i -lt 1) { write-warning "No files found in $($convertFromDir)" }
+	if(!($i -ge 1)) { write-warning "No files found in $($convertFromDir)" }
 }
