51d9f2095025579da6c17f315ddade189bfa0518
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-12-09 13:45:38 -0800

    Master

---
+++ Update-HostFQDN.ps1
@@ -0,0 +1,24 @@
+$computerName = $env:computername
+
+$DNSSuffix = "abc.com"
+
+$oldDNSSuffix = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\" -Name "NV Domain")."NV Domain"
+
+#Update primary DNS Suffix for FQDN
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\" -Name Domain -Value $DNSSuffix
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\" -Name "NV Domain" -Value $DNSSuffix
+
+#Update DNS Suffix Search List - Win8/2012 and above - if needed
+#Set-DnsClientGlobalSetting -SuffixSearchList $oldDNSSuffix,$DNSSuffix
+
+#Update AD's SPN records for machine if part of an AD domain
+if ((gwmi win32_computersystem).partofdomain -eq $true) {
+     $searchAD = new-object System.DirectoryServices.DirectorySearcher
+     $searchAD.filter = "(&amp;(objectCategory=computer)(cn=$($computerName)))"
+     $searchADItem = $searchAD.FindAll() | select -first 1
+     $adObj= [ADSI] $searchADItem.Path
+     $oldadObjSPN = $searchADItem.Properties.serviceprincipalname
+     $adObj.Put('serviceprincipalname',($oldadObjSPN -replace $oldDNSSuffix, $DNSSuffix))
+     $adObj.setinfo()
+     #$adObj.Get('serviceprincipalname')
+}
