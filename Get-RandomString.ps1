c2d77f802fd8157b996363b707e41d119f953a23
cajeeper <cajeeper@gmail.com>
2016-03-16 13:37:31 -0700

    Master

---
+++ Get-RandomString.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    Example of getting random string
+.DESCRIPTION
+    Already said IT! :P
+.NOTES
+    Author         : Justin Bennett (cajeeper@gmail.com)
+	Contact  : http://www.allthingstechie.net
+	Date           : 2016-03-16
+	Revision : v1.0
+	Changes  : 	v1.0 Original
+.LINK
+.PARAMETER ComputerName
+#>
+#random complex string, length of 10
+( 1..10 | % { "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-=_+[{}]\
+|,<>./?".ToCharArray() | Get-Random } ) -join ''
+
+#random basic string, length of 10
+( 1..10 | % { "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray() | Get-Random } ) -join ''
