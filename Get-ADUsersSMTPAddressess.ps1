a31637aa5054664711a5b67d07d7742fd45c32ce
cajeeper <cajeeper@gmail.com>
2017-02-01 15:33:11 -0800

    master

--- Get-ADUsersSMTPAddressess.ps1
+++ Get-ADUsersSMTPAddressess.ps1
@@ -1,5 +1,24 @@
-#email domain you want to see
-$emailDomain = "email.domain.you.want"
+<#  
+ .SYNOPSIS  
+  Simple script to export all of Active Directory SMTP addresses from
+  ProxyAddress property on User Objects
+    
+ .DESCRIPTION
+  Directory Searcher is used to find objects, then step through each users
+  array of Proxy Addresses and filter out only the SMTP address for a given
+  domain name. May be of use to dump list of addresses or just for licensing
+  a product the requires your SMTP addresses.
+ 
+ .NOTES   
+  Author   : Justin Bennett   
+  Date     : 2017-02-01
+  Contact  : http://www.allthingstechie.net
+  Revision : v1.0 
+  Changes  : v1.0 Original
+ #> 
+
+#email domain you want
+$emailDomain = "@email.domain.you.want"
 
 $searchAD = new-object System.DirectoryServices.DirectorySearcher
 $searchAD.PageSize = 1000000
@@ -9,11 +28,9 @@ $Users = $searchAD.FindAll()
 $emailSMTP =
 	foreach ($User in $Users){
 	
-		$proxyaddresses = $User.Properties.proxyaddresses
+		foreach ($proxyAddress in $User.Properties.proxyaddresses) {
 		
-		foreach ($proxyaddress in $proxyaddresses) {
-		
-		$proxyaddress | ?{$_.tolower() -match "smtp" -and $_.tolower() -match $emailDomain } | % {
+		$proxyAddress | ?{$_.tolower() -match "smtp" -and $_.tolower() -match $emailDomain } | % {
 		
 			New-Object PSObject -Property @{
 			
@@ -24,5 +41,8 @@ $emailSMTP =
 		}
 	}
 	
+#return unique SMTP addresses
 $emailSMTP | select smtp -unique
+
+#return SMTP count
 ($emailSMTP | select smtp -unique).count
